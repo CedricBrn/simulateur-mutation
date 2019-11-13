a8727c13d7c85a966a6260c32d1b6fccf7f22ac2
Brendan Chabannes <brendan.chabannes@gmail.com>
2019-11-13 12:24:01 +0100

    CSS pour impression

--- static/js/impression.js
+++ static/js/impression.js
@@ -3,14 +3,14 @@ function impression(aImprimer) {
     let fenetreImpression = window.open('', 'Impression', 'height=600,width=800');
 
     fenetreImpression.document.write('<html><head>');
-    fenetreImpression.document.write('<link rel="stylesheet" type="text/css" href="css/impression.css">');
+    fenetreImpression.document.write('<link rel="stylesheet" type="text/css" href="css/impression.css" media="print"> ');
     fenetreImpression.document.write('</head><body >');
     fenetreImpression.document.write(content);
     fenetreImpression.document.write('</body></html>');
 
     fenetreImpression.document.close();
-    fenetreImpression.focus()
-    fenetreImpression.print();
-    fenetreImpression.close();
+    // fenetreImpression.focus()
+    // fenetreImpression.print();
+    // fenetreImpression.close();
     return true;
 }
