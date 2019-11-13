ad3c43e7faf69e97b3f63979fcc97387565de0ad
Brendan Chabannes <brendan.chabannes@gmail.com>
2019-11-13 11:46:40 +0100

    mise en forme pour impression

--- static/js/impression.js
+++ static/js/impression.js
@@ -3,6 +3,7 @@ function impression(aImprimer) {
     let fenetreImpression = window.open('', 'Impression', 'height=600,width=800');
 
     fenetreImpression.document.write('<html><head>');
+    fenetreImpression.document.write('<link rel="stylesheet" type="text/css" href="css/impression.css">');
     fenetreImpression.document.write('</head><body >');
     fenetreImpression.document.write(content);
     fenetreImpression.document.write('</body></html>');
