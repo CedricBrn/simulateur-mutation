5cc5280cc5c2e3becadc0f83db68c7165f7b22e2
Brendan Chabannes <brendan.chabannes@gmail.com>
2019-11-15 18:36:19 +0100

    tentative asynchrone pour impression

--- static/js/impression.js
+++ static/js/impression.js
@@ -2,15 +2,14 @@ function impression(aImprimer) {
     let content = document.getElementById(aImprimer).innerHTML;
     let fenetreImpression = window.open('', 'Impression', 'height=600,width=800');
 
-    fenetreImpression.document.write('<html><head>');
-    fenetreImpression.document.write('<link rel="stylesheet" type="text/css" href="css/impression.css" media="print"> ');
-    fenetreImpression.document.write('</head><body >');
+    fenetreImpression.document.write('<html><head><link rel="stylesheet" type="text/css" href="css/impression.css"></head><body>');
     fenetreImpression.document.write(content);
     fenetreImpression.document.write('</body></html>');
-
     fenetreImpression.document.close();
-    fenetreImpression.focus()
-    fenetreImpression.print();
-    fenetreImpression.close();
+    fenetreImpression.focus();
+    fenetreImpression.addEventListener("load",fenetreImpression.print());
+    // fenetreImpression.print();
+    // fenetreImpression.close();
     return true;
 }
+
